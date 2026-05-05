10fbecf60ccbd7b60ea8ef3180bd0dbdacccfa63
Your Name <you@example.com>
2026-05-05 09:10:07 +0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::resource('posts', PostController::class);
+Route::get('posts', PostController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');;
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
